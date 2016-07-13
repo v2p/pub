09238a8f1e651a7a701e11a5fbeefe49ac27ed44
Vladimir Posvistelik <v.posvistelik@itransition.com>
2016-07-13 14:53:35 +0300

    Improved script for docker build with apt-cacher-ng usage

--- utils/build-with-apt-proxy.sh
+++ utils/build-with-apt-proxy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-# based on assumption that we have running apt-cacher-ng container:
-#APT_CACHER_NG_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' apt-cacher-ng)
-docker build --build-arg APT_PROXY=http://172.17.0.3:3142 $@
+CACHER_CONTAINER=apt-cacher-ng
+
+if [ -z "$(docker ps -f=\"name=${CACHER_CONTAINER}\" -q -a)" ]; then
+    docker run --name="${CACHER_CONTAINER}" -d -p 3142:3142 vposvistelik/${CACHER_CONTAINER}
+fi
+
+APT_CACHER_NG_IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CACHER_CONTAINER})
+
+docker build --build-arg APT_PROXY=http://${APT_CACHER_NG_IP}:3142 $@
