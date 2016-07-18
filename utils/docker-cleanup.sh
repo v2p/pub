d268498f5e9b7a8fa43e82017241c3b5fda4344d
Vladimir Posvistelik <v.posvistelik@itransition.com>
2016-07-18 13:14:27 +0300

    Utils: fix for docker-cleanup

--- utils/docker-cleanup.sh
+++ utils/docker-cleanup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker rm -v $(docker ps -a -q -f status=exited)
-docker rmi $(docker images -f "dangling=true" -q)
-docker volume rm $(docker volume ls -qf dangling=true)
+docker rm -v $(docker ps -aqf status=exited)
+docker rmi $(docker images -qf dangling=true)
+# docker volume rm $(docker volume ls -qf dangling=true)
