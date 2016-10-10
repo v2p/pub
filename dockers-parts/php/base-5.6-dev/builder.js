baab0af301b89ab2ce456748761f58eef161d99e
Vladimir Posvistelik <v.posvistelik@itransition.com>
2016-10-10 18:30:15 +0300

    Docker-parts development. Not finished yet.

---
+++ dockers-parts/php/base-5.6-dev/builder.js
@@ -0,0 +1,7 @@
+class Php56Builder {
+    build() {
+        return "5.6";
+    }
+}
+
+module.exports = new Php56Builder();
---
+++ dockers-parts/php/base-7.0-dev/builder.js
@@ -0,0 +1,7 @@
+class Php57Builder {
+    build() {
+        return "7.0";
+    }
+}
+
+module.exports = new Php57Builder();
